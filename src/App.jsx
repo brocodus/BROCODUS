dca545f7f01eb5bb53a82a97507abe6b2d85a057
brocodus <brocodus@gmail.com>
2025-09-13 14:09:17 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,8 @@
 import Home from '../src/components/Home'
 import Footer from '../src/components/Footer'
-import Navbar from '../src/components/Navbar'
-import { useEffect } from "react";
 import SocialOverView from './components/socialOverView';
 
 export default function App() {
-  useEffect(() => {
-    const navbar = document.querySelector(".navbar");
-    const home = document.querySelector("#home");
-
-    if (navbar && home) {
-      home.style.marginTop = navbar.offsetHeight + "px";
-    }
-  }, []);
 
   return (
     <>
